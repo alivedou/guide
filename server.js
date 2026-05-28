5017f60553be79c4a4c024f520c565c45ac39d5f
alivedou <alivedou@outlook.com>
2026-05-28 11:11:27 +0800

    修改部分内容

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -9,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '10mb' }));
 
